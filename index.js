47bab2e434327b1201ec678be96f6bc7eb335d88
jihoon <lanslot84@gmail.com>
2023-10-03 10:30:20 +0900

    update : fixed reply structure & render function

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 const commentSection = document.querySelector('#comments-section');
-const myName = "juliusomo";
+
 let commentData = {};
+let myName = "";
+
+function commentTemplate(originalCommentAuthor, item) {
+    console.log(originalCommentAuthor);
+    return `
+    <div class="comment-item set-flex" id="${item.id}">
+        <div class="col-rank set-flex direction-col">
+            <a class="btn rank-up" href="#">
+                <img src="./images/icon-plus.svg"/>
+            </a>
+            <div class="">${item.score}</div>
+            <a class="btn rank-down" href="#">
+                <img src="./images/icon-minus.svg"/>
+            </a>
+        </div>
+        <div class="col-content set-flex direction-col">
+            <div class="comment-author set-flex"> 
+                <div class="comment-author-info set-flex">
+                    <img class="comment-author-profile" src="${item.user.image.png}"/>
+                    <span class="comment-author-name">${item.user.username}</span>
+                    <span class="">${item.createdAt}</span>
+                </div>
+                <a class="comment-reply" id="${item.id}" href="#">
+                    <img src="./images/icon-reply.svg"/>
+                    <span>Reply</span>
+                </a>
+                <a class="comment-update" id="${item.id}" href="#">
+                    <img src="./images/icon-edit.svg"/>
+                    <span>Edit</span>
+                </a>
+                <a class="comment-delete" id="${item.id}" href="#">
+                    <img src="./images/icon-delete.svg"/>
+                    <span>Delete</span>
+                </a>
+            </div>
+            <div class="comment-content">
+                <span class="mention">${originalCommentAuthor}</span>
+                ${item.content}
+            </div>
+        </div>
+    </div>
+    `;
+}
 
 // ref - 페이지 로딩 후 실행 함수 : https://m.blog.naver.com/ka28/221991865312
 window.onload = function () {
@@ -8,44 +51,45 @@ window.onload = function () {
         .then(res => res.json() )
         .then(data => {
             commentData = data;
-            console.log(commentData.comments);
+            myName = data.currentUser.username;
+            
             renderComments(commentData.comments);
+            //console.log(commentData.comments);
         });
 };
-
 
 function renderComments(data) {
     let innerContents = "";
     Array.from(data).forEach( (item) => {
-        innerContents += 
-            `<div class="comment-item set-flex" id="${item.id}">
-                <div class="col-rank set-flex direction-col">
-                    <a class="btn rank-up" href="#">
-                        <img src="./images/icon-plus.svg"/>
-                    </a>
-                    <div class="">${item.score}</div>
-                    <a class="btn rank-down" href="#">
-                        <img src="./images/icon-minus.svg"/>
-                    </a>
-                </div>
-                <div class="col-content set-flex direction-col">
-                    <div class="comment-author set-flex"> 
-                        <div class="comment-author-info set-flex">
-                            <img class="comment-author-profile" src="${item.user.image.png}"/>
-                            <span class="comment-author-name">${item.user.username}</span>
-                            <span class="">${item.createdAt}</span>
-                        </div>
-                        <a class="comment-reply" id="${item.id}" href="#">
-                            <img src="./images/icon-reply.svg"/>
-                            <span>Reply</span>
-                        </a>
-                    </div>
-                    <div class="comment-content">
-                        ${item.content}
-                    </div>
-                </div>
-            </div>
-            `;
+        innerContents += commentTemplate(item.user.username, item);
+            // `<div class="comment-item set-flex" id="${item.id}">
+            //     <div class="col-rank set-flex direction-col">
+            //         <a class="btn rank-up" href="#">
+            //             <img src="./images/icon-plus.svg"/>
+            //         </a>
+            //         <div class="">${item.score}</div>
+            //         <a class="btn rank-down" href="#">
+            //             <img src="./images/icon-minus.svg"/>
+            //         </a>
+            //     </div>
+            //     <div class="col-content set-flex direction-col">
+            //         <div class="comment-author set-flex"> 
+            //             <div class="comment-author-info set-flex">
+            //                 <img class="comment-author-profile" src="${item.user.image.png}"/>
+            //                 <span class="comment-author-name">${item.user.username}</span>
+            //                 <span class="">${item.createdAt}</span>
+            //             </div>
+            //             <a class="comment-reply" id="${item.id}" href="#">
+            //                 <img src="./images/icon-reply.svg"/>
+            //                 <span>Reply</span>
+            //             </a>
+            //         </div>
+            //         <div class="comment-content">
+            //             ${item.content}
+            //         </div>
+            //     </div>
+            // </div>
+            // `;
     });
     commentSection.innerHTML = innerContents;
 
@@ -62,43 +106,43 @@ function renderReplies(data) {
     console.log(data);
     let innerContents = "";
     Array.from(data.replies).forEach( (item) => {
-        innerContents += 
-            `<div class="comment-item set-flex reply" id="${item.id}">
-                <div class="col-rank set-flex direction-col">
-                    <a class="btn rank-up" href="#">
-                        <img src="./images/icon-plus.svg"/>
-                    </a>
-                    <div class="">${item.score}</div>
-                    <a class="btn rank-down" href="#">
-                        <img src="./images/icon-minus.svg"/>
-                    </a>
-                </div>
-                <div class="col-content set-flex direction-col">
-                    <div class="comment-author set-flex"> 
-                        <div class="comment-author-info set-flex">
-                            <img class="comment-author-profile" src="${item.user.image.png}"/>
-                            <span class="comment-author-name">${item.user.username}</span>
-                            <span class="">${item.createdAt}</span>
-                        </div>
-                        <a class="comment-reply" id="${item.id}" href="#">
-                            <img src="./images/icon-reply.svg"/>
-                            <span>Reply</span>
-                        </a>
-                        <a class="comment-update" id="${item.id}" href="#">
-                            <img src="./images/icon-edit.svg"/>
-                            <span>Edit</span>
-                        </a>
-                        <a class="comment-delete" id="${item.id}" href="#">
-                            <img src="./images/icon-delete.svg"/>
-                            <span>Delete</span>
-                        </a>
-                    </div>
-                    <div class="comment-content">
-                        <span class="mention">${data.user.username}</span>${item.content}
-                    </div>
-                </div>
-            </div>
-            `;
+        innerContents += commentTemplate(data.user.username, item);
+            // `<div class="comment-item set-flex" id="${item.id}">
+            //     <div class="col-rank set-flex direction-col">
+            //         <a class="btn rank-up" href="#">
+            //             <img src="./images/icon-plus.svg"/>
+            //         </a>
+            //         <div class="">${item.score}</div>
+            //         <a class="btn rank-down" href="#">
+            //             <img src="./images/icon-minus.svg"/>
+            //         </a>
+            //     </div>
+            //     <div class="col-content set-flex direction-col">
+            //         <div class="comment-author set-flex"> 
+            //             <div class="comment-author-info set-flex">
+            //                 <img class="comment-author-profile" src="${item.user.image.png}"/>
+            //                 <span class="comment-author-name">${item.user.username}</span>
+            //                 <span class="">${item.createdAt}</span>
+            //             </div>
+            //             <a class="comment-reply" id="${item.id}" href="#">
+            //                 <img src="./images/icon-reply.svg"/>
+            //                 <span>Reply</span>
+            //             </a>
+            //             <a class="comment-update" id="${item.id}" href="#">
+            //                 <img src="./images/icon-edit.svg"/>
+            //                 <span>Edit</span>
+            //             </a>
+            //             <a class="comment-delete" id="${item.id}" href="#">
+            //                 <img src="./images/icon-delete.svg"/>
+            //                 <span>Delete</span>
+            //             </a>
+            //         </div>
+            //         <div class="comment-content">
+            //             <span class="mention">${data.user.username}</span>${item.content}
+            //         </div>
+            //     </div>
+            // </div>
+            // `;
     })
 
     // ref - 부모 자식 노드, 요소 찾기 : https://hianna.tistory.com/712
@@ -108,7 +152,8 @@ function renderReplies(data) {
     Array.from(commentSection.children).find( (targetItem) => {
         if(targetItem.id==data.id) {
             console.log(targetItem);
-            targetItem.insertAdjacentHTML('afterend', innerContents);
+            targetItem.insertAdjacentHTML('afterend', 
+                `<div class="reply set-flex direction-col">${innerContents}</div>`);
         }
     })
     
